9fb87070ec5594d8c6a6ce66381f6f7b244c7036
Sgradinaru1 <stefanj.gradinaru@utexas.edu>
2025-11-16 02:57:44 -0600

    Update memory.sv

--- RTL/memory.sv
+++ RTL/memory.sv
@@ -1,13 +1,5 @@
 `timescale 1ns / 1ps
-/*-----------------------------------------------------------------------------
-Author : UT Austin
-File   : memory.sv
-Create : 03/15/2025
-Revise : 03/15/2025
-Function:
-Serves as the block memory for the stack calculator. It takes 1 cycle to
-read/write data. There should be zero changes to the module.
------------------------------------------------------------------------------*/
+
 module memory(clock, cs, we, address, data_in, data_out);
     input clock;
     input cs;
@@ -27,3 +19,4 @@ module memory(clock, cs, we, address, data_in, data_out);
         data_out <= RAM[address];
     end
 endmodule
+
